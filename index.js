d9eae55757334de0e041731b74104853f237bf54
Mike McCarthy <mdmccarthyuk@gmail.com>
2020-05-26 10:47:10 +0100

    Added comments

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const news = require('./news.js')
 
+// Handle requests to '/' - return a generated headline
 app.get('/', (req, res) => {
     console.log('Fakenews processed a request');
     headline = news.generate_news();
@@ -9,6 +10,7 @@ app.get('/', (req, res) => {
     res.send(`<CENTER><H1>${(headline)}</H1></CENTER>`)
 });
 
+// Start the service listening
 const port = process.env.PORT || 8080;
 app.listen(port, () => {
     console.log('Fakenews listening on port ', port);
--- news.js
+++ news.js
@@ -7,22 +7,29 @@ var data_keys = new Array();
 
 console.log('init news');
 
+// Read the words list
 const rl_words = readline.createInterface({
     input: fs.createReadStream('./words'),
     crlfDelay: Infinity
 });
 
+// Populate the data_words object by building category keys and adding
+// words to an array under the appropriate category
 rl_words.on('line', (line) => {
     let temp_word=line.split("\t");
+    // If the category key doesn't exist add it and generate an empty array.
     if (typeof data_words[temp_word[1]] == 'undefined') {
-        data_words[temp_word[1]] = [];
+        data_words[temp_word[1]] = new Array();
     }
+    // Add the word to the object
     data_words[temp_word[1]].push(temp_word[0]);
+    // Add a category to the data_keys object if not already present
     if (!(data_keys.includes(temp_word[1]))) {
         data_keys.push(temp_word[1]);
     }
 })
 
+// Read the headlines file and populate an array with them.
 const rl_heads = readline.createInterface({
     input: fs.createReadStream('./heads'),
     crlfDelay: Infinity
@@ -32,11 +39,15 @@ rl_heads.on('line', (line) => {
     data_heads.push(line);
 })
 
+// Generates a headline based on the populated words and headlines objects.
 exports.generate_news = function () {
     let output = new String();
     let raw_headline = new String();
+    // Pick a random headline structure from the array
     raw_headline = data_heads[parseInt(Math.random()*data_heads.length)]
     let head_words = raw_headline.split(' ')
+    // Parse the words in the headline structure.  Where one starts with '['
+    // pull a random word from the named category.
     head_words.forEach(word => {
         if(word.startsWith('[')) {
             word = data_words[word.slice(1)][parseInt(Math.random()*data_words[word.slice(1)].length)];
@@ -49,5 +60,5 @@ exports.generate_news = function () {
 if (require.main === module) {
     // Called directly - trigger a test.
     let test = exports.generate_news();
-    console.log('lordy')
+    console.log(exports.generate_news());
 }
